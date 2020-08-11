64370ce3f67aef75c4a0aaf783f6cac6441e6fc8
ShaDream <russianboysasha@yandex.ru>
2020-08-11 16:13:52 +0500

    Made bubble property control

--- MangaTL/ViewModels/BubbleVM.cs
+++ MangaTL/ViewModels/BubbleVM.cs
@@ -1,13 +1,30 @@
 ﻿using System.Drawing;
+using System.Windows.Input;
 using MangaTL.Core;
+using Prism.Commands;
 using Prism.Mvvm;
+using Color = System.Windows.Media.Color;
 using Point = System.Windows.Point;
 
 namespace MangaTL.ViewModels
 {
     public class BubbleVM : BindableBase
     {
+        private const double FontSizes = 16;
+        private const double BorderSize = 5;
         private readonly TextBubble bubbleModel;
+
+        private readonly ImageViewerVM vm;
+
+        private double _backgroundOpacity;
+
+        private Color _borderColor;
+
+        private double _borderThickness;
+
+        private double _fontSize;
+
+        private double _foregroundOpacity;
         private double _height;
 
         private string _text;
@@ -16,8 +33,6 @@ namespace MangaTL.ViewModels
 
         private double _x;
         private double _y;
-
-        private ImageViewerVM vm;
 
         public double X
         {
@@ -49,27 +64,94 @@ namespace MangaTL.ViewModels
             set => SetProperty(ref _height, value);
         }
 
+        public ICommand MouseEnterCommand { get; set; }
+
+        public object MouseLeaveCommand { get; set; }
+
+        public Color BorderColor
+        {
+            get => _borderColor;
+            set => SetProperty(ref _borderColor, value);
+        }
+
+        public double BorderThickness
+        {
+            get => _borderThickness;
+            set => SetProperty(ref _borderThickness, value);
+        }
+
+        public double BackgroundOpacity
+        {
+            get => _backgroundOpacity;
+            set => SetProperty(ref _backgroundOpacity, value);
+        }
+
+        public double ForegroundOpacity
+        {
+            get => _foregroundOpacity;
+            set => SetProperty(ref _foregroundOpacity, value);
+        }
+
+        public double FontSize
+        {
+            get => _fontSize;
+            set => SetProperty(ref _fontSize, value);
+        }
+
         public BubbleVM(TextBubble bubbleModel, double scale, Point offset, ImageViewerVM vm)
         {
             this.bubbleModel = bubbleModel;
             this.vm = vm;
+            BorderThickness = BorderSize;
+            ForegroundOpacity = 0;
+            BackgroundOpacity = 0;
+            FontSize = FontSizes;
+            MouseEnterCommand = new DelegateCommand(() =>
+            {
+                BorderColor = Color.FromRgb(69, 123, 157);
+                ForegroundOpacity = 1;
+                BackgroundOpacity = 0.75;
+            });
+            MouseLeaveCommand = new DelegateCommand(() =>
+            {
+                BorderColor = Color.FromRgb(230, 57, 70);
+                ForegroundOpacity = 0;
+                BackgroundOpacity = 0;
+            });
+            BorderColor = Color.FromRgb(230, 57, 70);
+
             Text = bubbleModel.TextContent;
             UpdateVisual(scale, offset);
         }
 
         public void UpdateVisual(double scale, Point offset)
         {
+            FontSize = FontSizes * scale;
+            BorderThickness = BorderSize * scale;
             Width = bubbleModel.Rect.Width * scale;
             Height = bubbleModel.Rect.Height * scale;
             X = offset.X + bubbleModel.Rect.X * scale;
             Y = offset.Y + bubbleModel.Rect.Y * scale;
         }
 
-        public void SetNewRect(Rectangle newRect)
+        public void UpdateVisual()
         {
-            bubbleModel.Rect = newRect;
             var data = vm.GetData();
             UpdateVisual(data.Item2, data.Item1);
         }
+
+        public void SetNewRect(Rectangle newRect)
+        {
+            bubbleModel.Rect = newRect;
+            UpdateVisual();
+        }
+
+        public void SetNewText(string text)
+        {
+            bubbleModel.TextContent = text;
+            Text = text;
+        }
+
+        public TextBubble GetBubble => bubbleModel;
     }
 }
--- MangaTL/ViewModels/StyleControlVM.cs
+++ MangaTL/ViewModels/StyleControlVM.cs
@@ -1,8 +1,90 @@
-﻿using Prism.Mvvm;
+﻿using System.Drawing;
+using Prism.Mvvm;
 
 namespace MangaTL.ViewModels
 {
     public class StyleControlVM : BindableBase
     {
+        private BubbleVM _bubble;
+
+        private int _height;
+
+        private string _text;
+
+        private int _width;
+        private int _x;
+
+        private int _y;
+
+        public int X
+        {
+            get => _x;
+            set
+            {
+                if (_bubble == null)
+                    return;
+                SetProperty(ref _x, value);
+                _bubble.SetNewRect(new Rectangle(X, Y, Width, Height));
+            }
+        }
+
+        public int Y
+        {
+            get => _y;
+            set
+            {
+                if (_bubble == null)
+                    return;
+                SetProperty(ref _y, value);
+                _bubble.SetNewRect(new Rectangle(X, Y, Width, Height));
+            }
+        }
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_bubble == null)
+                    return;
+                SetProperty(ref _text, value);
+                _bubble.SetNewRect(new Rectangle(X, Y, Width, Height));
+            }
+        }
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                if (_bubble == null)
+                    return;
+                SetProperty(ref _width, value);
+                _bubble.SetNewText(Text);
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                if (_bubble == null)
+                    return;
+                SetProperty(ref _height, value);
+                _bubble.SetNewRect(new Rectangle(X, Y, Width, Height));
+            }
+        }
+
+        public void SetBubble(BubbleVM vm)
+        {
+            _bubble = vm;
+            var data = _bubble.GetBubble;
+            X = data.Rect.X;
+            Y = data.Rect.Y;
+            Width = data.Rect.Width;
+            Height = data.Rect.Height;
+            Text = data.TextContent;
+        }
     }
 }
